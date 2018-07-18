d109a4be26454ddec70f764845168d56d6491b2f
YeshiGlt <yeshimabet.geloto@gmail.com>
2018-07-18 16:35:48 +0200

    creation page twig

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -16,4 +16,28 @@ class DefaultController extends Controller
             'controller_name' => 'DefaultController',
         ]);
     }
+
+    public function accueil() {
+        return $this->render('default/accueil.html.twig');
+    }
+
+    public function concept() {
+        return $this->render('default/concept.html.twig');
+    }
+
+    public function proporeserv() {
+        return $this->render('default/proporeserv.html.twig');
+    }
+
+    public function profil() {
+        return $this->render('default/profil.html.twig');
+    }
+
+    public function amis() {
+        return $this->render('default/amis.html.twig');
+    }
+
+    public function contact() {
+        return $this->render('default/contact.html.twig');
+    }
 }
