219ecf83718c8f218f327c90a0874a6934832946
unknown <anp18072000@gmail.com>
2018-07-18 13:08:48 +0200

    migration ok

--- src/Migrations/Version20180718110754.php
+++ src/Migrations/Version20180718110754.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180718105929 extends AbstractMigration
+final class Version20180718110754 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
@@ -18,12 +18,11 @@ final class Version20180718105929 extends AbstractMigration
         $this->addSql('CREATE TABLE preferences (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, UNIQUE INDEX UNIQ_E931A6F5A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE proposition (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(100) NOT NULL, contenu VARCHAR(255) NOT NULL, date_heure DATETIME NOT NULL, nb_dispo VARCHAR(20) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE reservation (id INT AUTO_INCREMENT NOT NULL, proposition_id INT DEFAULT NULL, user_id INT NOT NULL, UNIQUE INDEX UNIQ_42C84955DB96F9E (proposition_id), UNIQUE INDEX UNIQ_42C84955A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, propositions_id INT DEFAULT NULL, prenom VARCHAR(100) NOT NULL, nom VARCHAR(100) NOT NULL, genre VARCHAR(1) NOT NULL, datenaissance DATE NOT NULL, pseudo VARCHAR(100) NOT NULL, email VARCHAR(250) NOT NULL, mdp VARCHAR(20) NOT NULL, mdp_confirm VARCHAR(20) NOT NULL, adresse VARCHAR(255) NOT NULL, ville VARCHAR(100) NOT NULL, cp VARCHAR(5) NOT NULL, phone VARCHAR(20) NOT NULL, INDEX IDX_8D93D649F0CBBA09 (propositions_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE preferences ADD CONSTRAINT FK_E931A6F5A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C84955DB96F9E FOREIGN KEY (proposition_id) REFERENCES proposition (id)');
         $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C84955A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE user ADD propositions_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649F0CBBA09 FOREIGN KEY (propositions_id) REFERENCES proposition (id)');
-        $this->addSql('CREATE INDEX IDX_8D93D649F0CBBA09 ON user (propositions_id)');
     }
 
     public function down(Schema $schema) : void
@@ -33,10 +32,11 @@ final class Version20180718105929 extends AbstractMigration
 
         $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C84955DB96F9E');
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649F0CBBA09');
+        $this->addSql('ALTER TABLE preferences DROP FOREIGN KEY FK_E931A6F5A76ED395');
+        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C84955A76ED395');
         $this->addSql('DROP TABLE preferences');
         $this->addSql('DROP TABLE proposition');
         $this->addSql('DROP TABLE reservation');
-        $this->addSql('DROP INDEX IDX_8D93D649F0CBBA09 ON user');
-        $this->addSql('ALTER TABLE user DROP propositions_id');
+        $this->addSql('DROP TABLE user');
     }
 }
