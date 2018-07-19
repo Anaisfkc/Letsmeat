a2ecac0558ecb3b57e68aaba24dd03d094cef107
YeshiGlt <yeshimabet.geloto@gmail.com>
2018-07-19 18:47:13 +0200

    creation plusieurs crud

---
+++ src/Form/ProfilType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Profil;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfilType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('saveurs')
+            ->add('pratiquefood')
+            ->add('prescriptionfood')
+            ->add('typefood')
+            ->add('recette')
+            ->add('intolerance')
+            ->add('score')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Profil::class,
+        ]);
+    }
+}
---
+++ src/Form/UserType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('prenom')
+            ->add('nom')
+            ->add('genre')
+            ->add('datenaissance')
+            ->add('pseudo')
+            ->add('email')
+            ->add('mdp')
+            ->add('mdpconfirm')
+            ->add('phone')
+            ->add('adresse')
+            ->add('ville')
+            ->add('cp')
+            ->add('proposition')
+            ->add('profil')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
